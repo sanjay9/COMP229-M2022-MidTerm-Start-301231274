b8db53c403f954bc08cf9341beed58189c416bbb
Sanjay <sanjaymahabir9@gmail.com>
2022-06-25 11:19:09 -0400

    Starting files commit

---
+++ Client/Scripts/app.ts
@@ -0,0 +1,28 @@
+//IIFE -- Immediately Invoked Function Expression
+"use strict";
+
+(function(){
+
+    function confirmDelete()
+    {
+      // confirm deletion
+      $("a.delete").on("click", function(event){
+        if(!confirm("Are you sure?"))
+        {
+          event.preventDefault();
+          location.href = '/books';
+        }       
+      });
+    }
+
+    function Start():void
+    {
+        console.log("App Started");
+        
+        confirmDelete();  
+    }
+
+    window.addEventListener("load", Start);
+
+})();
+
---
+++ Server/Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/books229";
+exports.RemoteURI = "your remote host information goes here";
+exports.HostName = "LocalHost";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
