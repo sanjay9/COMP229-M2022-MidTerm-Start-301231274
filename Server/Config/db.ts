cd12a2764bf17a80adcecb328e25db5752331d79
Sanjay <sanjaymahabir9@gmail.com>
2022-06-25 15:09:35 -0400

    added remote Uri environment variable

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://127.0.0.1/books229";
-export const RemoteURI = "mongodb+srv://sanjay:QfBS75JcjMyAtCdR@cluster0.neltn.mongodb.net/bookDB?retryWrites=true&w=majority"
+export const RemoteURI = process.env.URI;
 export const HostName = "LocalHost";
 export const Secret = "someSecret";
